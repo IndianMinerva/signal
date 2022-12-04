7aea2c1579240c53c70aa16dbd4594d9cb3f2602
Your Name <arun@Penguin.local>
2022-12-04 09:05:05 +0100

    Minor chnges

--- src/main/java/com/signal/signal/config/SignalConfig.java
+++ src/main/java/com/signal/signal/config/SignalConfig.java
@@ -1,6 +1,7 @@
 package com.signal.signal.config;
 
 import com.signal.signal.signal.Signal;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Configuration
+@Slf4j
 public class SignalConfig {
 
     @Autowired
@@ -18,6 +20,7 @@ public class SignalConfig {
 
     @Bean("signalRegister")
     public Map<Integer, Signal> signalMap() {
+        log.info("Signals in the registry: {}", signals.stream().map(Signal::getClass).toList());
         return signals.stream().collect(Collectors.toMap(Signal::getId, Function.identity()));
     }
 }
